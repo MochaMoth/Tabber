e37dab56e39d9dab8e608eae16e4eaba5a9dc4fc
Fedora Dev <fedora.game.dev@gmail.com>
2019-02-05 14:55:38 -0800

    Updated to be MORE automagic

--- Scripts/Tabber.cs
+++ Scripts/Tabber.cs
@@ -1,18 +1,24 @@
 ﻿using Malee;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tabber : MonoBehaviour
 {
-    [Reorderable] public GameObjects panels;
+    //[Reorderable] public GameObjects panels;
+    public GameObject tabs;
+    public GameObject panels;
     public int activePanel = 0;
     public bool retainPageOnDisable = false;
     public bool debugMode = false;
-
-    [System.NonSerialized]
-    public Tabs tabs;
     
     public virtual void Start()
     {
+        for (int i = 0; i < tabs.transform.childCount; i++)
+        {
+            int index = i;
+            tabs.transform.GetChild(i).GetComponent<Button>().onClick.AddListener(() => { CycleTo(index); });
+        }
+
         activePanel = 0;
         DeactivateAllPanels();
         ActivateActivePanel();
@@ -20,10 +26,10 @@ public class Tabber : MonoBehaviour
 
     public virtual void DeactivateAllPanels()
     {
-        foreach (GameObject gob in panels)
+        foreach (Transform tr in panels.transform)
         {
-            if (debugMode) Debug.Log(gob.name + " set inactive during DeactivateAllPanels()");
-            gob.SetActive(false);
+            if (debugMode) Debug.Log(tr.name + " set inactive during DeactivateAllPanels()");
+            tr.gameObject.SetActive(false);
         }
     }
 
@@ -31,8 +37,8 @@ public class Tabber : MonoBehaviour
     {
         try
         {
-            if (debugMode) Debug.Log(panels[activePanel].name + " set active during ActivateActivePanel().");
-            panels[activePanel].SetActive(true);
+            if (debugMode) Debug.Log(panels.transform.GetChild(activePanel).name + " set active during ActivateActivePanel().");
+            panels.transform.GetChild(activePanel).gameObject.SetActive(true);
         }
         catch
         {
@@ -43,9 +49,10 @@ public class Tabber : MonoBehaviour
     public virtual void CyclePanelForwards()
     {
         if (debugMode) Debug.Log("Cycling to next panel...");
-        if (debugMode) Debug.Log(panels[activePanel].name + " set inactive in CyclePanelForwards().");
-        panels[activePanel].SetActive(false);
-        activePanel = (activePanel + 1) % panels.Length;
+        if (debugMode) Debug.Log(panels.transform.GetChild(activePanel).name + " set inactive in CyclePanelForwards().");
+        panels.transform.GetChild(activePanel).gameObject.SetActive(false);
+        activePanel = (activePanel + 1) % panels.transform.childCount;
+        SetTab(activePanel);
 
         ActivateActivePanel();
     }
@@ -63,36 +70,38 @@ public class Tabber : MonoBehaviour
     public virtual void CyclePanelBackwards()
     {
         if (debugMode) Debug.Log("Cycling to previous panel...");
-        if (debugMode) Debug.Log(panels[activePanel].name + " set inactive in CyclePanelBackwards().");
-        panels[activePanel].SetActive(false);
+        if (debugMode) Debug.Log(panels.transform.GetChild(activePanel).name + " set inactive in CyclePanelBackwards().");
+        panels.transform.GetChild(activePanel).gameObject.SetActive(false);
         activePanel--;
         if (activePanel < 0)
-            activePanel = panels.Length - 1;
+            activePanel = panels.transform.childCount - 1;
+        SetTab(activePanel);
 
         ActivateActivePanel();
     }
 
     public virtual void CycleTo(int panel)
     {
-        if (panels.Length == 0)
+        if (panels.transform.childCount == 0)
         {
             if(debugMode) Debug.Log("There are no panels. Canceling cycle.");
             activePanel = 0;
             return;
         }
 
-        if (panel >= panels.Length || panel < 0)
+        if (panel >= panels.transform.childCount || panel < 0)
         {
-            if (debugMode) Debug.Log("Cannot cycle to panel " + panel + ". Available range: 0 - " + (panels.Length - 1) + ".");
+            if (debugMode) Debug.Log("Cannot cycle to panel " + panel + ". Available range: 0 - " + (panels.transform.childCount - 1) + ".");
             
-            while (panels.Length < panel && panel > 0)
+            while (panels.transform.childCount < panel && panel > 0)
                 panel--;
         }
 
-        if (debugMode) Debug.Log(panels[activePanel].name + " set inactive in CycleTo(" + panel + ").");
-        if (activePanel < panels.Length)
-            panels[activePanel].SetActive(false);
+        if (debugMode) Debug.Log(panels.transform.GetChild(activePanel).name + " set inactive in CycleTo(" + panel + ").");
+        if (activePanel < panels.transform.childCount)
+            panels.transform.GetChild(activePanel).gameObject.SetActive(false);
         activePanel = panel;
+        SetTab(activePanel);
 
         ActivateActivePanel();
     }
@@ -104,10 +113,21 @@ public class Tabber : MonoBehaviour
             if (debugMode) Debug.Log("UICycle disabled. Auto cycling back to first panel.");
             CycleTo(0);
             if (tabs != null)
-                tabs.SetTab(0);
+                SetTab(0);
         }
         else
             if (debugMode) Debug.Log("UICycle disabled. Retaining page user left off on.");
+    }
+
+    public void SetTab(int tabIndex)
+    {
+        for (int i = 0; i < tabs.transform.childCount; i++)
+        {
+            if (tabIndex == i)
+                tabs.transform.GetChild(i).GetComponent<Button>().interactable = false;
+            else
+                tabs.transform.GetChild(i).GetComponent<Button>().interactable = true;
+        }
     }
 }
 
